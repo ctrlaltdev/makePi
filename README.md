5e28067698b34c7e582a9d9afbab5fa2ab4db645
void <yorick@ctrlalt.dev>
2019-08-05 12:38:35 -0700

    Add README, License
    Add sanity check on n

---
+++ README.md
@@ -0,0 +1,14 @@
+[![licence](https://img.shields.io/github/license/ctrlaltdev/.svg?style=for-the-badge)](https://github.com/ctrlaltdev/makePi/blob/master/LICENCE.md)
+![Python](https://img.shields.io/badge/_-Python-4B8BBE.svg?style=for-the-badge)
+![JavaScript](https://img.shields.io/badge/_-JS-F0DB4F.svg?style=for-the-badge)
+
+A Pi API: https://pi.ctrlalt.dev/
+
+## Endpoints:
+### GET
+| Path | Returns |
+| --- | --- |
+| `/` | Pi with 100 decimals |
+| `/<n>` | Pi with n decimals |
+
+All endpoints returns json  
--- api.py
+++ api.py
@@ -24,9 +24,13 @@ def createRes(content, status = 200):
 
 @app.route('/<length>')
 def getPiN(length):
-    if (int(length) > 10000):
-        return createRes({ 'error': 'Awww, come on, that\'s too many decimals for a little server' }, 400)
-    return createRes({ 'pi': getPiDecimals(int(length)) })
+    try:
+        decimals = int(length)
+        if (decimals > 10000):
+            return createRes({ 'error': 'Awww, come on, that\'s too many decimals for a little server' }, 400)
+        return createRes({ 'pi': getPiDecimals(int(length)) })
+    except ValueError:
+        return createRes({ 'error': 'The number of decimals should be an int, DUH.' }, 400)
 
 @app.route('/')
 def getPi():
