89bf0b8a9829b6101ae2b0ca9a1577866bc25f37
ctrlaltdev <yorick@ctrlalt.dev>
2019-08-04 23:00:57 -0700

    Create simple py serve to serve n decimals of Pi

---
+++ server.py
@@ -0,0 +1,27 @@
+#!/bin/env python3
+
+from flask import Flask
+from pidigits import piGenerator
+
+app = Flask(__name__)
+
+def getPiDecimals(length = 100):
+    piGen = piGenerator()
+    next(piGen)
+    piDec = '3.'
+
+    for i in range(length):
+        piDec += str(next(piGen))
+
+    return piDec
+
+@app.route('/<length>')
+def getPiN(length):
+    return getPiDecimals(int(length))
+
+@app.route('/')
+def getPi():
+    return getPiDecimals()
+
+if __name__ == '__main__':
+    app.run()
